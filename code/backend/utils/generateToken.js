ea3484321c1a18f8bf46e410a432956880f1ab6c
NirakChoun <nirakchoun@gmail.com>
2025-10-31 12:28:37 -0500

    Updated JWT configuration

--- code/backend/utils/generateToken.js
+++ code/backend/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', 
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', 
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : underfined,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
