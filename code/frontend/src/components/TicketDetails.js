0f5bc12e1326378e42b60bfcbaddb968d7a17dfd
Gregory Hall <gjhall001@gmail.com>
2025-11-10 11:30:50 -0600

    Imported TicketDetails css to TicketDetails component

--- code/frontend/src/components/TicketDetails.js
+++ code/frontend/src/components/TicketDetails.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
+import "./TicketDetails.css";
 
 const API_BASE =
   process.env.NODE_ENV === "production"
